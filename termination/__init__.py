fe4a24a978330837e25354ff1f3551c8ee0f455b
Chris Bouchard <chris@upliftinglemma.net>
2018-08-09 23:35:33 -0400

    Pull term-related classes into terms.py
    They're pretty interdependent, so define them all together

--- termination/__init__.py
+++ termination/__init__.py
@@ -4,10 +4,18 @@ termination: A library for first-order term-rewriting
 
 __version__ = '0.0.2'
 
-__all__ = ['arity', 'ordering', 'Symbol', 'Term', 'Variable']
+__all__ = [
+    # orderings
+    'ordering',
 
-from .arities import arity
+    # terms
+    'Path',
+    'PathIterable',
+    'Symbol',
+    'Term',
+    'Variable',
+    'variables'
+]
+
 from .orderings import ordering
-from .symbols import Symbol
-from .terms import Term
-from .variables import Variable
+from .terms import Path, PathIterable, Symbol, Term, Variable, variables
--- termination/terms.py
+++ termination/terms.py
@@ -1,47 +1,94 @@
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, field
+from functools import singledispatch
 from typing import Iterable, Tuple
 
-from .arities import HasArity, arity
-
-__all__ = ['Path', 'PathIterable', 'Term']
+__all__ = ['Path', 'PathIterable', 'Symbol', 'Term', 'Variable', 'variables']
 
 
 Path = Tuple[int]
 PathIterable = Iterable[int]
 
 
+class TermLike(metaclass=ABCMeta):
+    @abstractmethod
+    def __getitem__(self, path: PathIterable) -> 'Term':
+        pass
+
+
 @dataclass(frozen=True)
-class Term:
-    root: HasArity
-    subterms: Tuple['Term'] = field(default_factory=tuple)
+class Symbol:
+    name: str
+    arity: int = field(default=0)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __call__(self, *args: 'Term') -> 'Term':
+        return Term(self, args)
+
+
+@dataclass(frozen=True)
+class Variable(TermLike):
+    name: str
+
+    def __str__(self) -> str:
+        return f'?{self.name}'
+
+    def __getitem__(self, path: PathIterable) -> TermLike:
+        # TODO: This syntax is ugly
+        for position in path:
+            raise IndexError(f'Invalid path: {original_path}')
+        return self
+
+
+@singledispatch
+def variables(value: Any) -> Iterable[Variable]:
+    if hasattr(value, _variables) and iscallable(value._variables):
+        return value._variables()
+    raise ValueError('Value does not have variables')
+
+
+@variables.register
+def _(variable: Variable) -> Iterable[Variable]:
+    yield variable
+
+
+@dataclass(frozen=True)
+class Term(TermLike):
+    root: Symbol
+    subterms: Tuple[TermLike] = field(default_factory=tuple)
 
     def __post_init__(self) -> None:
-        root_arity = arity(self.root)
+        arity = self.root.arity
         length = len(self.subterms)
         if root_arity != length:
             raise ValueError(
                 'Incorrect number of subterms: ' +
-                f'Expected {root_arity}, found {length}'
+                f'Expected {arity}, found {length}'
             )
 
     def __str__(self) -> str:
-        # TODO: Make name available in the typings
         root_name = self.root.name
 
-        if arity(self.root) == 0:
-            return str(self.root.name)
+        if self.root.arity == 0:
+            return str(root.name)
 
         subterms_str = ', '.join(str(subterm) for subterm in self.subterms)
         return f'{root_name}({subterms_str})'
 
-    def __getitem__(self, path: PathIterable) -> 'Term':
-        original_path = tuple(path)
+    def __getitem__(self, path: PathIterable) -> TermLike:
+        path_copy = tuple(path)
         term = self
 
-        for position in path:
+        for position in path_copy:
             try:
                 term = term.subterms[position]
             except IndexError as ex:
-                raise IndexError(f'Invalid path: {original_path}')
+                raise IndexError(f'Invalid path: {path_copy}')
 
         return term
+
+    def _variables(self) -> Iterable[Variable]:
+        for subterm in self.subterms:
+            yield from variables(subterm)
