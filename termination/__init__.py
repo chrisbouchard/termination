a0f77df7218ac2557c5861f6d75d5e5913b9d3c8
Chris Bouchard <chris@upliftinglemma.net>
2018-08-11 00:11:42 -0400

    Clean up __init__ imports/__all__

--- termination/__init__.py
+++ termination/__init__.py
@@ -4,18 +4,27 @@ termination: A library for first-order term-rewriting
 
 __version__ = '0.0.2'
 
+from .orderings import (
+    ordering
+)
+from .terms import (
+    Position,
+    PositionIterable,
+    Symbol,
+    Term,
+    Variable,
+    variables
+)
+
 __all__ = [
     # orderings
     'ordering',
 
     # terms
-    'Path',
-    'PathIterable',
+    'Position',
+    'PositionIterable',
     'Symbol',
     'Term',
     'Variable',
     'variables'
 ]
-
-from .orderings import ordering
-from .terms import Path, PathIterable, Symbol, Term, Variable, variables
