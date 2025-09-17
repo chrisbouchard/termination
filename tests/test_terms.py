6c9c89903961b91ae21b6467a601aef8bd52b8af
Chris Bouchard <chris@upliftinglemma.net>
2025-09-16 22:28:42 -0400

    Rewrite term tests using pytest

--- tests/test_terms.py
+++ tests/test_terms.py
@@ -4,7 +4,7 @@
 # TODO: Test in on different objects
 # TODO: Test Constant
 
-from unittest import TestCase
+import pytest
 
 from termination.terms import (
     Constant,
@@ -17,229 +17,256 @@ from termination.terms import (
 )
 
 
-class TestFunction(TestCase):
-    """Test case for the Function class."""
+class TestFunction:
+    """Test cases for the Function class."""
 
     def test_call(self):
         """A Function symbol can be called to create a Term."""
         f = Function(name="f", arity=2)
         x = Variable(name="x")
         y = Variable(name="y")
-        term = f(x, y)
 
-        self.assertIsInstance(term, Term)
+        assert isinstance(f(x, y), Term)
+
+    def test_call_root(self):
+        """Calling a function symbol creates a term with the symbol as the root."""
+        f = Function(name="f", arity=2)
+        x = Variable(name="x")
+        y = Variable(name="y")
+
+        assert f(x, y).root == f
+
+    def test_call_children(self):
+        """Calling a function symbol creates a term with the given children."""
+        f = Function(name="f", arity=2)
+        x = Variable(name="x")
+        y = Variable(name="y")
+
+        assert f(x, y).children == (x, y)
 
 
-class TestVariable(TestCase):
-    """Test case for the Variable class."""
+class TestVariable:
+    """Test cases for the Variable class."""
 
     def test_str(self):
         """A Variable formats as a string."""
         x = Variable(name="x")
-        self.assertEqual(str(x), "?x")
+        assert str(x) == "?x"
 
     def test_getitem_root(self):
         """A Variable is its own root subterm."""
         x = Variable(name="x")
-        self.assertIs(x[()], x)
+        assert x[()] is x
 
     def test_getitem_invalid(self):
         """A Variable has no non-root position."""
         x = Variable(name="x")
-        with self.assertRaises(KeyError):
+        with pytest.raises(KeyError):
             x[(0,)]
 
     def test_subterms(self):
         """A Variable supports iterating its subterms."""
         x = Variable(name="x")
-        expected_subterm_set = {((), x)}
-        actual_subterm_set = set(x.subterms())
-
-        self.assertEqual(expected_subterm_set, actual_subterm_set)
+        assert set(x.subterms()) == {((), x)}
 
     def test_positions(self):
         """A Variable supports iterating its positions."""
         x = Variable(name="x")
-        expected_position_set = {()}
-        actual_position_set = set(x.positions())
-
-        self.assertEqual(expected_position_set, actual_position_set)
+        assert set(x.positions()) == {()}
 
     def test_variables(self):
         """A Variable supports iterating its variables."""
         x = Variable(name="x")
-        expected_variables = {x}
-        actual_variables = set(variables(x))
-
-        self.assertEqual(expected_variables, actual_variables)
+        assert set(variables(x)) == {x}
 
 
-class TestIndexedVariable(TestCase):
-    """Test case for the IndexedVariable class."""
+class TestIndexedVariable:
+    """Test cases for the IndexedVariable class."""
 
     def test_str(self):
         """An IndexedVariable formats as a string."""
         x = IndexedVariable(name="x", index=1)
-        self.assertEqual(str(x), "?x#1")
+        assert str(x) == "?x#1"
 
     def test_getitem_root(self):
         """An IndexedVariable is its own root subterm."""
         x = IndexedVariable(name="x", index=1)
-        self.assertIs(x[()], x)
+        assert x[()] is x
 
     def test_getitem_invalid(self):
         """An IndexedVariable has no non-root position."""
         x = IndexedVariable(name="x", index=1)
-        with self.assertRaises(KeyError):
+        with pytest.raises(KeyError):
             x[(0,)]
 
     def test_subterms(self):
         """An IndexedVariable supports iterating its subterms."""
         x = IndexedVariable(name="x", index=1)
-        expected_subterm_set = {((), x)}
-        actual_subterm_set = set(x.subterms())
-
-        self.assertEqual(expected_subterm_set, actual_subterm_set)
+        assert set(x.subterms()) == {((), x)}
 
     def test_positions(self):
         """An IndexedVariable supports iterating its positions."""
         x = IndexedVariable(name="x", index=1)
-        expected_position_set = {()}
-        actual_position_set = set(x.positions())
-
-        self.assertEqual(expected_position_set, actual_position_set)
+        assert set(x.positions()) == {()}
 
     def test_variables(self):
         """An IndexedVariable supports iterating its variables."""
         x = IndexedVariable(name="x", index=1)
-        expected_variables = {x}
-        actual_variables = set(variables(x))
-
-        self.assertEqual(expected_variables, actual_variables)
+        assert set(variables(x)) == {x}
 
 
-class TestTerm(TestCase):
-    """Test case for the Term class."""
+class TestTerm:
+    """Test cases for the Term class."""
 
-    def setUp(self):
-        """Set up useful stuff for testing."""
-        self.f = Function("f", 2)
-        self.g = Function("g", 1)
+    f = Function("f", 2)
+    g = Function("g", 1)
 
-        self.x = Variable("x")
-        self.y = Variable("y")
-        self.z = Variable("z")
+    x = Variable("x")
+    y = Variable("y")
+    z = Variable("z")
 
-        self.g_subterm = Term(root=self.g, children=(self.x,))
-
-        self.f_subterm = Term(root=self.f, children=(self.y, self.z))
-
-        self.term = Term(root=self.f, children=(self.g_subterm, self.f_subterm))
-
-    def test_getitem_root(self):
+    @pytest.mark.parametrize(
+        ("term", "expected_str"),
+        [
+            pytest.param(g(x), "g(?x)"),
+            pytest.param(f(y, z), "f(?y, ?z)"),
+            pytest.param(f(g(x), f(y, z)), "f(g(?x), f(?y, ?z))"),
+        ],
+    )
+    def test_str(self, term, expected_str):
         """A Term is its own root subterm."""
-        self.assertIs(self.term[()], self.term)
+        assert str(term) == expected_str
 
-    def test_getitem_subterm(self):
+    @pytest.mark.parametrize(
+        ("term",),
+        [
+            pytest.param(g(x)),
+            pytest.param(f(y, z)),
+            pytest.param(f(g(x), f(y, z))),
+        ],
+    )
+    def test_getitem_root(self, term):
+        """A Term is its own root subterm."""
+        assert term[()] is term
+
+    @pytest.mark.parametrize(
+        ("term", "position", "expected_subterm"),
+        [
+            pytest.param(g(x), (0,), x),
+            pytest.param(f(y, z), (0,), y),
+            pytest.param(f(y, z), (1,), z),
+            pytest.param(f(g(x), f(y, z)), (0,), g(x)),
+            pytest.param(f(g(x), f(y, z)), (0, 0), x),
+            pytest.param(f(g(x), f(y, z)), (1,), f(y, z)),
+            pytest.param(f(g(x), f(y, z)), (1, 0), y),
+            pytest.param(f(g(x), f(y, z)), (1, 1), z),
+        ],
+    )
+    def test_getitem_subterm(self, term, position, expected_subterm):
         """A Term supports getting its subterms by position."""
-        self.assertEqual(self.term[(0,)], self.g_subterm)
-        self.assertEqual(self.term[(0, 0)], self.x)
-        self.assertEqual(self.term[(1,)], self.f_subterm)
-        self.assertEqual(self.term[(1, 0)], self.y)
-        self.assertEqual(self.term[(1, 1)], self.z)
+        assert term[position] == expected_subterm
 
-    def test_getitem_invalid(self):
+    @pytest.mark.parametrize(
+        ("term", "position"),
+        [
+            pytest.param(f(g(x), f(y, z)), (2,)),
+            pytest.param(f(g(x), f(y, z)), (0, 1)),
+            pytest.param(f(g(x), f(y, z)), (1, 2)),
+            pytest.param(f(g(x), f(y, z)), (0, 0, 1)),
+        ],
+    )
+    def test_getitem_invalid(self, term, position):
         """A Term does not support getting invalid positions."""
-        with self.assertRaises(KeyError):
-            self.term[(2,)]
-        with self.assertRaises(KeyError):
-            self.term[(0, 1)]
-        with self.assertRaises(KeyError):
-            self.term[(1, 2)]
-        with self.assertRaises(KeyError):
-            self.term[(0, 0, 1)]
+        with pytest.raises(KeyError):
+            term[position]
 
-    def test_subterms(self):
+    @pytest.mark.parametrize(
+        ("term", "expected_subterms"),
+        [
+            pytest.param(
+                f(g(x), f(y, z)),
+                {
+                    ((), f(g(x), f(y, z))),
+                    ((0,), g(x)),
+                    ((0, 0), x),
+                    ((1,), f(y, z)),
+                    ((1, 0), y),
+                    ((1, 1), z),
+                },
+            )
+        ],
+    )
+    def test_subterms(self, term, expected_subterms):
         """A Term supports iterating its subterms."""
-        expected_subterm_set = {
-            ((), self.term),
-            ((0,), self.g_subterm),
-            ((0, 0), self.x),
-            ((1,), self.f_subterm),
-            ((1, 0), self.y),
-            ((1, 1), self.z),
-        }
+        assert set(term.subterms()) == expected_subterms
 
-        actual_subterm_set = set(self.term.subterms())
-
-        self.assertEqual(expected_subterm_set, actual_subterm_set)
-
-    def test_positions(self):
+    @pytest.mark.parametrize(
+        ("term", "expected_positions"),
+        [
+            pytest.param(
+                f(g(x), f(y, z)),
+                {(), (0,), (0, 0), (1,), (1, 0), (1, 1)},
+            )
+        ],
+    )
+    def test_positions(self, term, expected_positions):
         """A Term supports iterating its positions."""
-        expected_position_set = {(), (0,), (0, 0), (1,), (1, 0), (1, 1)}
+        assert set(term.positions()) == expected_positions
 
-        actual_position_set = set(self.term.positions())
-
-        self.assertEqual(expected_position_set, actual_position_set)
-
-    def test_variables(self):
+    @pytest.mark.parametrize(
+        ("term", "expected_variables"),
+        [
+            pytest.param(f(g(x), f(y, z)), {x, y, z}),
+        ],
+    )
+    def test_variables(self, term, expected_variables):
         """A Term supports iterating its variables."""
-        expected_variables = {self.x, self.y, self.z}
-        actual_variables = set(variables(self.term))
-
-        self.assertEqual(expected_variables, actual_variables)
+        assert set(variables(term)) == expected_variables
 
 
-class TestSubstitution(TestCase):
+class TestSubstitution:
     """Test case for the Substitution class."""
 
-    def setUp(self) -> None:
-        """Set up useful stuff for testing."""
-        self.f = Function("f", 2)
-        self.g = Function("g", 1)
+    f = Function("f", 2)
+    g = Function("g", 1)
 
-        self.a = Constant("a")
-        self.b = Constant("b")
-        self.c = Constant("c")
+    a = Constant("a")
+    b = Constant("b")
+    c = Constant("c")
 
-        self.x = Variable("x")
-        self.y = Variable("y")
-        self.z = Variable("z")
+    x = Variable("x")
+    y = Variable("y")
+    z = Variable("z")
 
-    def test_empty_str(self):
-        """An empty substitution formats as a string."""
-        sub = Substitution()
-        expected_str = "{}"
-        actual_str = str(sub)
+    @pytest.mark.parametrize(
+        ("sub", "expected_str"),
+        [
+            pytest.param(Substitution(), "{}"),
+            pytest.param(
+                Substitution({x: f(x, y), y: g(z)}),
+                "{?x -> f(?x, ?y), ?y -> g(?z)}",
+            ),
+        ],
+    )
+    def test_str(self, sub, expected_str):
+        """A Substitution formats as a string."""
+        assert str(sub) == expected_str
 
-        self.assertEqual(expected_str, actual_str)
-
-    def test_nonempty_str(self):
-        """A nonempty substitution formats as a string."""
-        sub = Substitution(
-            mapping={self.x: self.f(self.x, self.y), self.y: self.g(self.z)}
-        )
-        expected_str = "{?x -> f(?x, ?y), ?y -> g(?z)}"
-        actual_str = str(sub)
-
-        self.assertEqual(expected_str, actual_str)
-
-    def test_substitute(self):
-        """A substitution applies to another substitution."""
-        inner_sub = Substitution(
-            mapping={self.x: self.f(self.x, self.y), self.y: self.g(self.z)}
-        )
-        outer_sub = Substitution(
-            mapping={self.x: self.a, self.y: self.b, self.z: self.c}
-        )
-
-        expected_sub = Substitution(
-            mapping={
-                self.x: self.f(self.a, self.b),
-                self.y: self.g(self.c),
-                self.z: self.c,
-            }
-        )
-        actual_sub = outer_sub(inner_sub)
-
-        self.assertEqual(expected_sub, actual_sub)
+    @pytest.mark.parametrize(
+        ("sub", "target", "expected"),
+        [
+            pytest.param(
+                Substitution({x: a, y: b, z: c}),
+                f(g(x), f(y, z)),
+                f(g(a), f(b, c)),
+            ),
+            pytest.param(
+                Substitution({x: a, y: b, z: c}),
+                Substitution({x: f(x, y), y: g(z)}),
+                Substitution({x: f(a, b), y: g(c), z: c}),
+            ),
+        ],
+    )
+    def test_substitute(self, sub, target, expected):
+        """Calling a substitution applies it to the given value."""
+        assert sub(target) == expected
