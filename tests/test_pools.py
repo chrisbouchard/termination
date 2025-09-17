ce4b49b1fa016c27a41a9d145dc732fa911c67de
Chris Bouchard <chris@upliftinglemma.net>
2025-09-17 00:10:15 -0400

    Rewrite variable pool tests in pytest

--- tests/test_pools.py
+++ tests/test_pools.py
@@ -1,111 +1,107 @@
 """Unit tests for the termination.pools module."""
 
-from unittest import TestCase
+import pytest
 
 from termination.pools import VariablePool, fresh_variable
 from termination.terms import IndexedVariable, Variable
 
 
-class TestPool(TestCase):
+class TestPool:
     """Test case for the VariblePool class."""
 
-    def test_create(self):
-        """A variable pool can be instantiated."""
-        VariablePool()
-
-    def test_get(self):
+    @pytest.mark.parametrize(
+        ("name",),
+        [pytest.param("x"), pytest.param("y")],
+    )
+    def test_get_variable(self, name):
         """A VariablePool can get variables."""
         pool = VariablePool()
+        variable = pool.get(name)
+        assert isinstance(variable, Variable)
+        assert variable.name == name
 
-        x = pool.get("x")
-        y = pool.get("y")
-        z1 = pool.get("z", 1)
-        z2 = pool.get("z", 2)
+    @pytest.mark.parametrize(
+        ("name", "index"),
+        [
+            pytest.param("x", 1),
+            pytest.param("x", 2),
+            pytest.param("y", 1),
+            pytest.param("y", 2),
+        ],
+    )
+    def test_get_indexed_variable(self, name, index):
+        """A VariablePool can get variables."""
+        pool = VariablePool()
+        variable = pool.get(name, index)
+        assert isinstance(variable, IndexedVariable)
+        assert variable.name == name
+        assert variable.index == index
 
-        self.assertIsInstance(x, Variable)
-        self.assertEqual("x", x.name)
-        self.assertIsInstance(y, Variable)
-        self.assertEqual("y", y.name)
-        self.assertIsInstance(z1, IndexedVariable)
-        self.assertEqual("z", z1.name)
-        self.assertEqual(1, z1.index)
-        self.assertIsInstance(z2, IndexedVariable)
-        self.assertEqual("z", z2.name)
-        self.assertEqual(2, z2.index)
-
-    def test_getitem(self):
+    @pytest.mark.parametrize(
+        ("name",),
+        [pytest.param("x"), pytest.param("y")],
+    )
+    def test_getitem(self, name):
         """A VariablePool can get variables using bracket ['x'] notation."""
         pool = VariablePool()
+        variable = pool[name]
+        assert isinstance(variable, Variable)
+        assert variable.name == name
 
-        x = pool["x"]
-        y = pool["y"]
-
-        self.assertIsInstance(x, Variable)
-        self.assertEqual("x", x.name)
-        self.assertIsInstance(y, Variable)
-        self.assertEqual("y", y.name)
-
-    def test_getitem_singleton(self):
+    @pytest.mark.parametrize(
+        ("name",),
+        [pytest.param("x"), pytest.param("y")],
+    )
+    def test_getitem_singleton(self, name):
         """A VariablePool returns the same variable instance."""
         pool = VariablePool()
+        variable1 = pool[name]
+        variable2 = pool[name]
+        assert variable1 is variable2
 
-        x1 = pool["x"]
-        y1 = pool["y"]
-        x2 = pool["x"]
-        y2 = pool["y"]
-
-        self.assertIs(x1, x2)
-        self.assertIs(y1, y2)
-
-    def test_fresh(self):
+    @pytest.mark.parametrize(
+        ("name",),
+        [pytest.param("x"), pytest.param("y")],
+    )
+    def test_fresh(self, name):
         """A VariablePool can generate "fresh" variables.
 
         Fresh variables have a unique index from any other index of a variable
         with the same name returned from that VariablePool.
         """
         pool = VariablePool()
+        variable1 = pool.get_fresh(name)
+        variable2 = pool.get_fresh(name)
 
-        x1 = pool.get_fresh("x")
-        x2 = pool.get_fresh("x")
-        y1 = pool.get_fresh("y")
-        x3 = pool.get_fresh("x")
-        y2 = pool.get_fresh("y")
+        assert isinstance(variable1, IndexedVariable)
+        assert variable1.name == name
+        assert variable1.index == 1
 
-        self.assertIsInstance(x1, IndexedVariable)
-        self.assertEqual("x", x1.name)
-        self.assertEqual(1, x1.index)
-        self.assertIsInstance(x2, IndexedVariable)
-        self.assertEqual("x", x2.name)
-        self.assertEqual(2, x2.index)
-        self.assertIsInstance(x3, IndexedVariable)
-        self.assertEqual("x", x3.name)
-        self.assertEqual(3, x3.index)
-        self.assertIsInstance(y1, IndexedVariable)
-        self.assertEqual("y", y1.name)
-        self.assertEqual(1, y1.index)
-        self.assertIsInstance(y2, IndexedVariable)
-        self.assertEqual("y", y2.name)
-        self.assertEqual(2, y2.index)
+        assert isinstance(variable2, IndexedVariable)
+        assert variable2.name == name
+        assert variable2.index == 2
 
-    def test_independent_pools(self):
+    @pytest.mark.parametrize(
+        ("name",),
+        [pytest.param("x"), pytest.param("y")],
+    )
+    def test_independent_pools(self, name):
         """Different VariablePools have independent fresh indices."""
         pool1 = VariablePool()
         pool2 = VariablePool()
 
-        x1_1 = pool1.get_fresh("x")
-        x1_2 = pool1.get_fresh("x")
-        x2_1 = pool2.get_fresh("x")
-        x1_3 = pool1.get_fresh("x")
-        x2_2 = pool2.get_fresh("x")
+        variable1_pool1 = pool1.get_fresh(name)
+        variable1_pool2 = pool2.get_fresh(name)
+        variable2_pool1 = pool1.get_fresh(name)
+        variable2_pool2 = pool2.get_fresh(name)
 
-        self.assertEqual(1, x1_1.index)
-        self.assertEqual(2, x1_2.index)
-        self.assertEqual(3, x1_3.index)
-        self.assertEqual(1, x2_1.index)
-        self.assertEqual(2, x2_2.index)
+        assert variable1_pool1.index == 1
+        assert variable1_pool2.index == 1
+        assert variable2_pool1.index == 2
+        assert variable2_pool2.index == 2
 
 
-class TestFreshVariable(TestCase):
+class TestFreshVariable:
     """Test case for the fresh_variabe function."""
 
     def test_fresh_variable(self):
@@ -116,12 +112,13 @@ class TestFreshVariable(TestCase):
         x1 = fresh_variable(x)
         x2 = fresh_variable(x)
 
-        self.assertIsInstance(x1, IndexedVariable)
-        self.assertEqual("x", x1.name)
-        self.assertEqual(1, x1.index)
-        self.assertIsInstance(x2, IndexedVariable)
-        self.assertEqual("x", x2.name)
-        self.assertEqual(2, x2.index)
+        assert isinstance(x1, IndexedVariable)
+        assert x1.name == "x"
+        assert x1.index == 1
+
+        assert isinstance(x2, IndexedVariable)
+        assert x2.name == "x"
+        assert x2.index == 2
 
     def test_indexed_variable(self):
         """The fresh_variable function works on indexed variables from a pool."""
@@ -131,19 +128,13 @@ class TestFreshVariable(TestCase):
         x1 = fresh_variable(x)
         x2 = fresh_variable(x1)
 
-        pool.get("x", 10)
+        assert isinstance(x1, IndexedVariable)
+        assert x1.name == "x"
+        assert x1.index == 1
 
-        x11 = fresh_variable(x1)
-
-        self.assertIsInstance(x1, IndexedVariable)
-        self.assertEqual("x", x1.name)
-        self.assertEqual(1, x1.index)
-        self.assertIsInstance(x2, IndexedVariable)
-        self.assertEqual("x", x2.name)
-        self.assertEqual(2, x2.index)
-        self.assertIsInstance(x11, IndexedVariable)
-        self.assertEqual("x", x11.name)
-        self.assertEqual(11, x11.index)
+        assert isinstance(x2, IndexedVariable)
+        assert x2.name == "x"
+        assert x2.index == 2
 
     def test_fresh_pool(self):
         """The fresh_variable function works on a VariablePool."""
@@ -152,31 +143,30 @@ class TestFreshVariable(TestCase):
         v1 = fresh_variable(pool)
         v2 = fresh_variable(pool)
 
-        self.assertIsInstance(v1, IndexedVariable)
-        self.assertEqual("", v1.name)
-        self.assertEqual(1, v1.index)
-        self.assertIsInstance(v2, IndexedVariable)
-        self.assertEqual("", v2.name)
-        self.assertEqual(2, v2.index)
+        assert isinstance(v1, IndexedVariable)
+        assert v1.name == ""
+        assert v1.index == 1
 
-    def test_fresh_throws(self):
+        assert isinstance(v2, IndexedVariable)
+        assert v2.name == ""
+        assert v2.index == 2
+
+    @pytest.mark.parametrize(
+        ("target",),
+        [
+            pytest.param(Variable("x")),
+            pytest.param(IndexedVariable("y", 1)),
+            pytest.param(27),
+        ],
+    )
+    def test_fresh_throws(self, target):
         """The fresh_variable function throws for non-pool objects.
 
         For variables that were not generated by a VariablePool, and for other
         random objects, fresh_variable should raise a ValueError.
         """
-        x = Variable("x")
-        y1 = IndexedVariable("y", 1)
-        z = 5
-
-        with self.assertRaises(ValueError):
-            fresh_variable(x)
-
-        with self.assertRaises(ValueError):
-            fresh_variable(y1)
-
-        with self.assertRaises(ValueError):
-            fresh_variable(z)
+        with pytest.raises(ValueError):
+            fresh_variable(target)
 
     def test_fresh_shares_index(self):
         """The fresh_variable function uses the same fresh index as get_fresh."""
@@ -187,6 +177,6 @@ class TestFreshVariable(TestCase):
         x2 = pool.get_fresh("x")
         x3 = fresh_variable(x)
 
-        self.assertEqual(1, x1.index)
-        self.assertEqual(2, x2.index)
-        self.assertEqual(3, x3.index)
+        assert x1.index == 1
+        assert x2.index == 2
+        assert x3.index == 3
